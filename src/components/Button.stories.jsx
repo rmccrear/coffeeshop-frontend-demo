2e757c290ac369cf125f93d26f4489babcceb3ed
Robert McCreary Mannino <fooyinfighter@gmail.com>
2025-01-06 18:36:02 -0800

    install storybook

---
+++ src/components/Button.stories.jsx
@@ -0,0 +1,25 @@
+import Button from './Button';
+
+const meta = {
+  component: Button,
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    label: "Buy Now!!!!!!!"
+  }
+};
+
+export const BuyLater = {
+  args: {
+    label: "Buy Later ~_~"
+  }
+};
+
+export const BuyTwo = {
+  args: {
+    label: "Buy Two!!"
+  }
+};
