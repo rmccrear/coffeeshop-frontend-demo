72535712c593427a308816fef4ee0b13486661a0
Robert McCreary Mannino <fooyinfighter@gmail.com>
2025-01-13 19:32:52 -0800

    add functionality for add to cart

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,14 +1,25 @@
+import { useState, useEffect } from "react";
 import ProductCard from "@/components/ProductCard";
 import CartItem from "@/components/CartItem";
-import cart from "@/mocks/cart.json";
+// import cart from "@/mocks/cart.json";
+import { 
+  loadCartFromLocalStorage,
+  saveCartToLocalStorage } from '@/utils';
 
 
 // Instead of add to cart, 
 // we will have a remove from cart button
 
 export default function Cart() {
-  const cartContents = cart.products;
-  console.log(cartContents)
+  const [cartContents, setCartContents] = useState([]);
+  // const cartContents = cart.products;
+  // console.log(cartContents)
+
+  useEffect(() => {
+    // TODO: Load Cart from the server...
+    const cartData = loadCartFromLocalStorage();
+    setCartContents(cartData);
+  }, []);
 
   const cartJSX = cartContents.map((product) => {
     // Use key prop every time you use map.
--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,26 +1,36 @@
-// import Navbar from "@/components/Navbar";
-// import Footer from "@/components/Footer";
-
-// export default function ProductsPage() {
-//   return (
-//     <div className="container mx-auto px-4">
-//       <Navbar />
-//       <h1>Products Page</h1>
-//       <Footer content="Coffee is great" />
-//     </div>
-//   );
-// }
+import { useState, useEffect } from 'react';
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Loading from '@/components/Loading';
 
 import ProductCard from '@/components/ProductCard';
-import products from '@/mocks/products.json';
+import productData from '@/mocks/products.json';
+import { 
+    loadCartFromLocalStorage,
+    saveCartToLocalStorage } from '@/utils';
 
-// const products = [
-//   { _id: 1, name: 'Coffee A', description: 'Rich and smooth.', price: 10, image: '/coffee-a.jpg' },
-//   { _id: 2, name: 'Coffee B', description: 'Dark roast.', price: 12, image: '/coffee-b.jpg' },
-// ];
 
 export default function ProductsPage() {
-  
+  const [products, setProducts] = useState([]);
+  const [cartContents, setCartContents] = useState([]);
+
+  useEffect(() => {
+
+    // Load cart from local storage
+    const cartData = loadCartFromLocalStorage(); // get data from outside the component
+    setCartContents(cartData); // set data inside the component
+
+    setProducts(productData);
+  }, []);
+
+  function addProductToCart(product) {
+    // just like...
+    // cartContents.push(product);
+    const newCartContents = [ ...cartContents, product ];
+    setCartContents(newCartContents);
+
+    saveCartToLocalStorage(newCartContents);
+  }
   const productsJSX = products.map((product) => {
     // Use key prop every time you use map.
     // This is a unique identifier for each product.
@@ -28,18 +38,24 @@ export default function ProductsPage() {
     // so we need to give it help by providing a unique key prop.
     function addToCart() {
       alert("clicked add to cart " + product.name);
+      addProductToCart( product );
     }
-    return (<ProductCard 
-              key={product._id}
-              product={product}
-              addToCart={addToCart}
-            />)
+    return (<ProductCard
+      key={product._id}
+      product={product}
+      addToCart={addToCart}
+    />)
   });
   return (
-    <div className="grid grid-rows-3 grid-flow-col">
-      {
-        productsJSX
-      }
+    <div>
+      <Navbar />
+      <div className="grid grid-rows-3 grid-flow-col">
+        <Loading isLoading={false} />
+        {
+          productsJSX
+        }
+      </div>
+      <Footer />
     </div>
   );
 }
