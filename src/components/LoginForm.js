dafeb1884bf462d05dd964af470d27ebdddefabf
Robert McCreary Mannino <fooyinfighter@gmail.com>
2025-01-29 15:14:19 -0800

    update login page

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,13 +4,18 @@ import Button from '@/components/Button';
 export default function LoginForm({ buttonLabel, handleLogin}) {
   function handleSubmit(event) {
     event.preventDefault();
-    handleLogin();
+    // Get email and password from FORM
+    console.log(event.target.elements.email);
+    const email = event.target.elements.email.value;
+    const password = event.target.elements.password.value;
+
+    handleLogin(email, password);
   }
   return (
-    <form className="form" onSubmit={handleSubmit}>
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <Button label={buttonLabel} />
+    <form className="form flex flex-col" onSubmit={handleSubmit}>
+      <input type="email" placeholder="Email" name="email"/>
+      <input type="password" placeholder="Password" name="password" />
+      <Button label={buttonLabel}/>
     </form>
   );
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import { useRouter } from 'next/router';
 import LoginForm from '@/components/LoginForm';
 import Navbar from '@/components/Navbar';
@@ -5,14 +6,50 @@ import Footer from '@/components/Footer';
 
 export default function LoginPage() {
   const router = useRouter();
-  function handleLogin() {
-    alert('Login clicked!');
-    router.push('/products');
+  const [ error, setError ] = useState(false); 
+  const [ errorMessage, setErrorMessage ] = useState("");
+
+  async function handleLogin(email, password) {
+    alert('Login clicked! ' + email);
+    // fetch POST
+    //       to url: https://coffee-shop-backend-5fmn.onrender.com/api/v2/users/login
+    const payload = { email, password };
+
+    // const response = await fetch("https://coffee-shop-backend-5fmn.onrender.com/api/v2/users/login", {
+    // const response = await fetch("http://localhost:4000/api/v2/users/login", {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/users/login`, {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    });
+    const data = await response.json();
+    console.log(data);
+    console.log(data.token);
+
+    // Save the token in local storage...
+    if(data.token && data.user) {
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("user", JSON.stringify(data.user));
+      // if successful, go to products.
+      setError(false);
+      router.push('/products');
+    } else {
+      setError(true);
+      setErrorMessage(data.error);
+    }
   }
+
   return (
     <div>
       <Navbar />
       <h1>Login Up</h1>
+      {error && (
+        <div className="text-red-400 text-sm">
+          Error: {errorMessage}
+        </div>
+      )}
       <LoginForm buttonLabel="Login" handleLogin={handleLogin}/>
       <Footer />
     </div>
